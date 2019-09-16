028e3afc6e104e6a271e68b5cb2e216f0a8ede7d
Harish Kumar <harish.kumar10@t-mobile.com>
2019-09-16 17:22:14 +0530

    addtional changes for numeric directive

--- src/app/shared/directives/numeric-only.directive.ts
+++ src/app/shared/directives/numeric-only.directive.ts
@@ -1,13 +1,17 @@
-import { Directive, OnInit } from '@angular/core';
+import { Directive, OnInit, Input, ElementRef} from '@angular/core';
 
 @Directive({
-  selector:'numeric-only'
+  selector:'[numeric-only]'
 })
 
 export class NumericOnlyDirective implements OnInit{
 
-	constructor(){}
- 
+	constructor(elem : ElementRef){
+	console.log('element '+elem);
+	 elem.nativeElement.onkeydown=(evnt)=>{
+	 	console.log('tester'+evnt);
+	 }	
+	}
    ngOnInit(){
    	console.log('numeric-only directive loaded');
    }
