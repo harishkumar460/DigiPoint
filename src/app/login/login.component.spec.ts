689cba45a7925e930e253299066c5a48b9a082f0
Harry <hari63jp@gmail.com>
2019-11-17 00:41:37 +0530

    login screen test cases

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,14 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+//import { NgxUiLoaderModule} from  'ngx-ui-loader';
+//import { ApiService } from '../services/api.service';
 import { LoginComponent } from './login.component';
-
+import { HttpClientModule } from '@angular/common/http';
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent],
+      providers: [],
+      imports: [FormsModule,ReactiveFormsModule,RouterTestingModule,
+      HttpClientModule]
     })
     .compileComponents();
   }));
@@ -19,7 +24,10 @@ describe('LoginComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('invoke process login function', () => {
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    component.processLogin();
+    expect(component.showSpinner).toEqual(true);
   });
 });
