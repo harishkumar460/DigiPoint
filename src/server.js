4ca8b4f0504a48b2106dbe1b4c45b7b5c504eea8
Harry <hari63jp@gmail.com>
2019-12-06 00:06:49 +0530

    deploy changes

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+app.set('port', (process.env.PORT || 5000));
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 8080);
+//app.listen(process.env.PORT || 8080);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
