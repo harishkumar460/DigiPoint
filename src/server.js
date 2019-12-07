a7c7a5d6992109e933821d02aec01f8b891ecff5
Harry <hari63jp@gmail.com>
2019-12-07 16:26:44 +0530

    app local server run success

--- src/server.js
+++ src/server.js
@@ -7,8 +7,17 @@ app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
 //app.listen(process.env.PORT || 8080);
-app.get('/', function(request, response) {
-  response.render('/index');
+/*app.get('/', function(request, response) {
+  response.render('/index.html');
+});*/
+const app_folder = 'dist/myDemoApp';
+
+// ---- SERVE STATIC FILES ---- //
+app.get('*.*', express.static(app_folder, {maxAge: '1y'}));
+
+// ---- SERVE APLICATION PATHS ---- //
+app.all('*', function (req, res) {
+    res.status(200).sendFile(`/`, {root: app_folder});
 });
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
